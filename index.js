d9fe9cce4ba45ad48e89f441ac369f4b6f709f2a
Zoe Monet Williams <mwilliamszoe@gmail.com>
2020-01-07 02:23:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// * `overwriteBestCustomer()`: See the consequences of declaring a variable in global scope, by writing a new function called `overwriteBestCustomer()` that changes that `bestCustomer` variable.
 // * `leastFavoriteCustomer` and `changeLeastFavoriteCustomer()`: Now declare a constant in global scope called `leastFavoriteCustomer`, be sure to assign it some initial value. Ok, now write a function called `changeLeastFavoriteCustomer()` that attempts to change that constant - notice what JavaScript does when you try to change the constant.
 
 var customerName = 'bob';
@@ -12,4 +11,8 @@ function setBestCustomer(){
 
 function overwriteBestCustomer(){
   bestCustomer = 'maybe bob'
+}
+
+function leastFavoriteCustomer(){
+  
 }
